f8c2b5c1c3cc11b7cc623d3ac0e6be8f442dc551
Krtin Shet <krtinshet.dev@gmail.com>
2023-12-22 23:09:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Container>
+      <Typography variant="h1">Namira, Happy Learning react 💓</Typography>
+      <Button variant="contained">Let's Go 🚀</Button>
+    </Container>
+  );
+}
+
+export default App;
